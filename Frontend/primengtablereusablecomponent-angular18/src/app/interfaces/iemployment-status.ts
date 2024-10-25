411fd2eb199a2b701f74f850da47412b7f61f70c
AIO1 <alejandroibrahimojea@gmail.com>
2024-10-25 17:19:02 +0200

    Removed example project for Angular 14 and .NET 6

--- Frontend/primengtablereusablecomponent-angular18/src/app/interfaces/iemployment-status.ts
+++
@@ -1,8 +0,0 @@
-// This is only needed for the example project
-export interface IEmploymentStatus {
-    id: string;
-    statusName: string;
-    colorR: number;
-    colorG: number;
-    colorB: number;
-}
