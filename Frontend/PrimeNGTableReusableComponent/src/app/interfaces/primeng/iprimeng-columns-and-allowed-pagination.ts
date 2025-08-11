07602236e978e80dbd84e136c6b689c30c5a552d
AIO1 <alejandroibrahimojea@gmail.com>
2025-08-11 17:55:11 +0200

    Added base project

--- Frontend/PrimeNGTableReusableComponent/src/app/interfaces/primeng/iprimeng-columns-and-allowed-pagination.ts
+++
@@ -1,8 +0,0 @@
-import { IprimengColumnsMetadata } from "./iprimeng-columns-metadata";
-export interface IprimengColumnsAndAllowedPagination {
-    columnsInfo: IprimengColumnsMetadata[];
-    allowedItemsPerPage: number[];
-    dateFormat: string;
-    dateTimezone: string;
-    dateCulture: string;
-}
